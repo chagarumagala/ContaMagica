e62144e93f3304256e8e6c09673e8330b6dadfe2
Hiram Artnak Martins <hiram.martins@edu.pucrs.br>
2020-04-25 19:26:25 -0300

    fixed pom

--- src/test/java/com/froggyChair/ContaSpec.java
+++ src/test/java/com/froggyChair/ContaSpec.java
@@ -4,10 +4,11 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class ContaSpec {
+    //This tests verifies 
     @Test
-    // This tests verifies if a new account starts empty
     public void accountStartsEmpty(){
         ContaMagica conta = new ContaMagica();
         assertTrue(conta.getSaldo() == 0, () -> "A conta deve começar com saldo = 0");
     }
+    // This test verifies 
 }
