e68d347d0ad5e4aa10aca3bb1914a8a8f8057162
Pedro Carlucci <pkcarlucci@gmail.com>
2020-04-25 20:04:16 -0300

    getStatus

--- src/main/java/com/froggyChair/ContaMagica.java
+++ src/main/java/com/froggyChair/ContaMagica.java
@@ -2,19 +2,21 @@ package com.froggyChair;
 
 public class ContaMagica {
     private double saldo;
+    private int status;
     public static final int SILVER = 0;
     public static final int GOLD = 1;
     public static final int PLATINUM = 2;
 
     public ContaMagica(){
         this.saldo = 0;
+        this.status = 0;
     }
     public double getSaldo(){
         return saldo;
     }
     int getStatus()
     {
-        return 1;
+        return status;
     }
     public void deposito(int valor) throws INVALID_OPER_EXCEPTION
     {
