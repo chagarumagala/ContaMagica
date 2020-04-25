315684c0b73ebceecfea8c6cd4bbde5a74300650
Elise <ivanscheibelprezzi@gmail.com>
2020-04-25 19:48:31 -0300

    adicionada exceção

--- src/main/java/com/froggyChair/ContaMagica.java
+++ src/main/java/com/froggyChair/ContaMagica.java
@@ -2,10 +2,29 @@ package com.froggyChair;
 
 public class ContaMagica {
     private double saldo;
+    public static final int SILVER = 0;
+    public static final int GOLD = 1;
+    public static final int PLATINUM = 2;
+
     public ContaMagica(){
         this.saldo = 0;
     }
     public double getSaldo(){
         return saldo;
+    }
+    int getStatus()
+    {
+        return 1;
+    }
+    public void deposito(int valor) throws INVALID_OPER_EXCEPTION
+    {
+        if (valor<0)
+        {
+            throw new INVALID_OPER_EXCEPTION("valor invalido");
+        }
+    }
+    public void retirada(int valor) throws INVALID_OPER_EXCEPTION
+    {
+
     }
 }
---
+++ src/main/java/com/froggyChair/INVALID_OPER_EXCEPTION.java
@@ -0,0 +1,8 @@
+package com.froggyChair;
+
+public class INVALID_OPER_EXCEPTION extends Exception{
+    public INVALID_OPER_EXCEPTION(String errorMessage) {
+        super(errorMessage);
+    }
+
+}
